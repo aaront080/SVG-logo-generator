5e4d0e9fbc303313a549b13a7e8f8743253e7310
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-10-24 21:46:44 -0700

    example logo generated

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const prompts = require('./lib/prompts.js');
 const createShape = require('./lib/createShape.js');
-const createLogo = ("./examples/logo.svg");
+const filePath = "./examples/logo.svg";
 
 
 function generateLogo(response) {
-    const finalLogo = createShape(response);
-    fs.writeFile(createLogo, finalLogo, () => console.log('logo.svg has been generated'));
+    const userLogo = createShape(response);
+    fs.writeFile(filePath, userLogo, () => console.log('logo.svg has been generated'));
 }
 
-function init() {
+function run() {
     inquirer
     .prompt(prompts)
     .then((response) => {
@@ -21,7 +21,7 @@ function init() {
     });
 }
 
-init()
+run()
 
 
 
@@ -30,36 +30,9 @@ init()
 
 
 
-/*inquirer
-    .prompt([
-        {
-            type: "input",
-            name: "letter",
-            message: "Enter up to 3 letters for your logo.",
-        },
-        {
-            type: "input",
-            name: "text",
-            message: "Enter preferred color or hex# for the text color."
-        },
-        {
-            type: "list",
-            name: "shape",
-            message: "Select your logo shape.",
-            choices: ["circle", "triangle", "square"]
-        },
-        {
-            type: "input",
-            name: "logo",
-            message: "Enter preferred color or hex# for the logo color."
-        },     
-    ])
-    .then((data) => {
-        const logoFile = "./examples/logo.svg";
-        const generatedLogo = createShape(data);
 
-        fs.writeFile(logoFile,createShape(generatedLogo), (err) =>
-        err ? console.log(err) : console.log("Logo.svg has been generated")
-        );
-    });
-    .catch((err) => console.error(err)); */
+
+
+
+
+
