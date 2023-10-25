d112988764abc8697b600a36367f8ebfae5b6661
Aaron Torres <143736506+aaront080@users.noreply.github.com>
2023-10-25 16:39:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const prompts = require('./lib/prompts.js');
 const createShape = require('./lib/createShape.js');
 const filePath = "./examples/logo.svg";
 
-
+//function that will generate logo and write file to logo.svg
 function generateLogo(response) {
     const userLogo = createShape(response);
     fs.writeFile(filePath, userLogo, () => console.log('logo.svg has been generated'));
 }
+
+//function that initializes the prompts and generate logo function
 
 function run() {
     inquirer
